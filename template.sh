cf60120a649d77bbffca6f2594449c4198a285b8
nada <nadamainkai@gmail.com>
2025-11-21 23:25:54 +0100

    folder structure added

---
+++ template.sh
@@ -0,0 +1,16 @@
+#generate folders
+mkdir -p src
+mkdir -p research
+
+#generate files
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt_template.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trials.ipynb
+touch requirements.txt
+
+
+echo "directory and files structure created."
